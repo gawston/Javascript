7ffe2f5664e6efc0a925cbe6f27154d952aa9c9b
gawston <naruenat.privatez@gmail.com>
2023-12-06 11:35:51 +0700

    lecture02

---
+++ lecture02/EXCERCISE/Ex01.js
@@ -0,0 +1,8 @@
+function findPerimeter(length, width) {
+    let result = length * 2 + width * 2;
+    return result;
+}
+
+console.log(findPerimeter(6, 7));
+console.log(findPerimeter(20, 10));
+console.log(findPerimeter(2, 9));
---
+++ lecture02/EXCERCISE/Ex02.js
@@ -0,0 +1,11 @@
+function equation(str) {
+    let result = str.split('');
+    console.log(result);
+    for(let i = 0; i < str.length; i++) {
+
+    }
+}
+
+// equation("1+1");
+// equation("7*4-2");
+// equation("1+1+1+1+1");
